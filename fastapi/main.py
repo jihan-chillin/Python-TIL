94f4f10a5ba1304c151451275ca5221f61eeea00
jihan <jihan@jihanui-MacBookPro.local>
2023-01-26 07:53:11 +0900

    enum class

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,23 +1,21 @@
-from typing import Union
+from enum import Enum
 
 from fastapi import FastAPI
-from pydantic import BaseModel
 
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+    
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer:Union[bool, None] = None
 
-@app.get("/")
-def read_root():
-    return {"Hello" : "World"}
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id:int, q:Union[str,None]=None):
-    return {"item_id" : item_id, "q" : q}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
 
-@app.put("item/{item_id}")
-def update_item(item_id: int, item:Item):
-    return {"item_name" : item.name, "item_id" : item_id}
+    return {"model_name": model_name, "message": "Have some residuals"}
