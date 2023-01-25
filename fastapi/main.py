d22cbac39784926643082188ffce1847eb856ac6
jihan <jihan@jihanui-MacBookPro.local>
2023-01-26 08:14:29 +0900

    request body

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,17 +1,19 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
 
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
-    item = {"item_id": item_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
+@app.post("/items/")
+async def create_item(item: Item):
     return item
