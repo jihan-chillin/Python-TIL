a8a17504f53c286f52915085eefb16471520c204
jihan <jihan@jihanui-MacBookPro.local>
2023-01-25 17:13:20 +0900

    number type

---
+++ 4_1_number.py
@@ -0,0 +1,19 @@
+print(-1);
+print(0)
+print(1) # int
+print(1.1) #float
+
+
+print(1+1)
+print('1+1', 1+1)
+print('2-1', 2-1)
+print('2*1', 2*1)
+print('4/2', 4/2)
+
+import math # 수한연산 관련 모듈 import
+
+print('math.sqrt(4)', math.sqrt(4))
+print('math.pow(4)', math.pow(4,2))
+
+import random # 랜덤 모듈 import
+print(random.random())
