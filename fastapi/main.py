23b0f7ee65d73feee62fd1e597c68d5b8b610a77
jihan <jihan@jihanui-MacBookPro.local>
2023-01-26 08:36:48 +0900

    numeric validation

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,23 +1,16 @@
 from typing import Union
 
-from fastapi import FastAPI
-from pydantic import BaseModel
-
-
-class Item(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
-
+from fastapi import FastAPI, Path, Query
 
 app = FastAPI()
 
 
-@app.post("/items/")
-async def create_item(item: Item):
-    item_dict = item.dict()
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
+@app.get("/items/{item_id}")
+async def read_items(
+    item_id: int = Path(title="The ID of the item to get"),
+    q: Union[str, None] = Query(default=None, alias="item-query"),
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
