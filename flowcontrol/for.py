445e54e37f0f97df20cd7d7e00a001584cebff5a
jihan <jihan@jihanui-MacBookPro.local>
2023-01-25 18:10:58 +0900

    다차원 for

---
+++ flowcontrol/for.py
@@ -0,0 +1,18 @@
+# names = ['jihan', 'chillin', 'kozub']
+
+# for i in names:
+#     print('hi...' + i + '...bye')
+    
+#  다차원 배열
+
+
+persons = [
+    ['jihan', 'seoul', 'coding'],
+    ['jihan22', 'shinchon', 'coffee'],
+    ['jihan33', 'changcheon', 'diet']
+]
+
+name, address, interest = ['jihan', 'seoul', 'coding']
+
+for name, address, interest in persons : 
+    print(name + ' ' + address + ' ' + interest)
