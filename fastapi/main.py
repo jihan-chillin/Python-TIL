d3230565e9f703a91cce550577d603a0257a86fe
jihan <jihan@jihanui-MacBookPro.local>
2023-01-26 08:02:18 +0900

    paging

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
-app = FastAPI();
+app = FastAPI()
 
-@app.get('/files/{file_path:path}')
-async def read_file(file_path:str):
-    return {"file_path" : file_path }
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
