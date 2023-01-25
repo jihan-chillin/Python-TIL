d7842fb8004e0c81fadb93a1ec5b9839a970cbea
jihan <jihan@jihanui-MacBookPro.local>
2023-01-26 08:39:52 +0900

    multiple parameter

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,16 +1,24 @@
 from typing import Union
 
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def read_items(
-    item_id: int = Path(title="The ID of the item to get"),
-    q: Union[str, None] = Query(default=None, alias="item-query"),
-):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+
+class User(BaseModel):
+    username: str
+    full_name: Union[str, None] = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, user: User):
+    results = {"item_id": item_id, "item": item, "user": user}
     return results
