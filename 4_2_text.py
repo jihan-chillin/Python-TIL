e3c7e034e60567752105047044d45617a25872dd
jihan <jihan@jihanui-MacBookPro.local>
2023-01-25 17:18:53 +0900

    text type

---
+++ 4_2_text.py
@@ -0,0 +1,10 @@
+print('hello world')
+print('''hello 
+world''')
+
+
+print("'1'+'1'", '1'+'1')
+print('hello wolrd' * 10)
+print("length : ",len('hello wolrd' * 10))
+
+print('hello world'.replace('world', 'universe'))
